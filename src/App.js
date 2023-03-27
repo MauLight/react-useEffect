c9691f011eef253fadefb5d0fa09747cdac8522a
MauLight <maulisseluz@gmail.com>
2023-03-27 14:31:29 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [toggle, setToggle] = useState(false);
+  const [title, setTitle] = useState("Using the useEffect hook");
+
+  const handleClick = () => {
+    setToggle(!toggle);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{title}</h1>
+      <button onClick={handleClick} className="btn border rounded-0" ></button>
     </div>
   );
 }
